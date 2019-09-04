b098620011ce2287dd85ef05a31b80f0c226aa0e
Luka Bartolic <lukabartolic@farshore.com>
2019-09-04 16:21:50 +0200

    updates

--- source.php
+++ source.php
@@ -2,10 +2,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+require __DIR__ . '/vendor/autoload.php';
+
 include 'RecursiveDOMIterator.php';
 use PhpContentParser\RecursiveDOMIterator;
+use GuzzleHttp\Client;
 
-const DELIMITER = '<w>';
+const DELIMITER = '<i>';
+const DELIMITER_CLOSING = '</i>';
+const BLUEPRINT_PLACEHOLDER_FORMAT = '[%d]';
+const GOOGLE_TRANSLATE_API_KEY = 'AIzaSyBtjj5CX1whbkPNOiwPvxNSDFfC5_FWS2s';
+const GOOGLE_TRANSLATE_ENDPOINT = 'https://translation.googleapis.com/language/translate/v2';
 
 $dom = new DomDocument();
 
@@ -16,141 +23,121 @@ $dom->loadHTMLFile($htmlPath);
 $elements = [];
 $outputBlueprint = '';
 $body = $dom->getElementsByTagName('body');
-$xpath = new DOMXpath($dom);
 
-/*foreach ($xpath->evaluate('//text()') as $node) {
-    $node->nodeValue = "a";
+$domBlueprint = createOutputBlueprint($dom);
+$translationStr = prepareTranslationString(getDOMElementsArray($dom, true));
+$translated = translate($translationStr, 'hr', 'en');
+$output = restoreTranslationContent($translated, $domBlueprint);
+var_dump($output);
+
+function translate($translationString, $source, $target) : string {
+    $data = [
+        "q"             => $translationString,
+        "source"        => "en",
+        "target"        => "hr",
+        "format"        => "html"
+    ];
+    $httpClient = new Client();
+    $headers = [
+        'Content-Type'  => 'application/json'
+    ];
+    $response = $httpClient->request('POST', GOOGLE_TRANSLATE_ENDPOINT, [
+        'query'         => ['key' => GOOGLE_TRANSLATE_API_KEY],
+        'json'          => $data,
+        'headers'       => $headers
+    ]);
+    
+    $responseJson = json_decode($response->getBody());
+    $translated = str_replace(DELIMITER_CLOSING, '', $responseJson->data->translations[0]->translatedText);
+    return $translated;
 }
-var_dump($dom->saveHtml($dom->getElementsByTagName('body')[0]));
-die();*/
 
-$domClone = new DOMDocument();
-$domClone = $dom->cloneNode(true);
-
-/*if ($body && $body->length > 0) {
-    foreach($body[0]->childNodes as $key => $element) {
-        $domClone->appendChild($domClone->importNode($element, true));
-    }
-}*/
-
-$bodyClone = $domClone->getElementsByTagName('body');
-
-$testDoc = new DOMDocument();
-$testI = 0;
-if ($bodyClone && $bodyClone->length > 0) {
-    foreach($bodyClone[0]->childNodes as $key => $element) {
-        if ($element->hasChildNodes()) {
-            $domIterator = new RecursiveIteratorIterator(
-                new RecursiveDOMIterator($element), RecursiveIteratorIterator::SELF_FIRST
-            );
-            foreach($domIterator as $iteratorKey => $iteratorNode) {
-                if ($iteratorNode->nodeName == '#text') {
-                    $iteratorNode->nodeValue = $testI;
-                    $testI++;
+function createOutputBlueprint(DOMDocument $dom) : DOMDocument {
+    $domClone = $dom->cloneNode(true);
+    $bodyClone = $domClone->getElementsByTagName('body');
+    $domBlueprint = new DOMDocument();
+    $placeholderIndex = 0;
+    $placeholderFormat = BLUEPRINT_PLACEHOLDER_FORMAT;
+    
+    if ($bodyClone && $bodyClone->length > 0) {
+        foreach($bodyClone[0]->childNodes as $key => $element) {
+            if ($element->hasChildNodes()) {
+                $domIterator = new RecursiveIteratorIterator(
+                    new RecursiveDOMIterator($element), RecursiveIteratorIterator::SELF_FIRST
+                );
+                foreach($domIterator as $iteratorNode) {
+                    if ($iteratorNode->nodeName == '#text') {
+                        $iteratorNode->nodeValue = sprintf($placeholderFormat, $placeholderIndex);
+                        $placeholderIndex++;
+                    }
                 }
             }
-
-            //$testDoc->appendChild($testDoc->importNode($element, true));
+        }
+        foreach($bodyClone[0]->childNodes as $key => $element) {
+            $domBlueprint->appendChild($domBlueprint->importNode($element, true));
         }
     }
-    foreach($bodyClone[0]->childNodes as $key => $element) {
-        $testDoc->appendChild($testDoc->importNode($element, true));
-    }
+
+    return $domBlueprint;
 }
-print_r($testDoc->saveHTML());
 
-/*$testDoc->appendChild($testDoc->importNode($body->item(0), true));
-print_r($testDoc->getElementsByTagName('body')->item(0)->nodeValue);
-die();*/
-
-
-$testIndex = 0;
-if ($body && $body->length > 0) {
-    foreach($body[0]->childNodes as $key => $element) {
+function getDOMElementsArray(DOMDocument $dom, $extractBody = false) : array {
+    if ($extractBody == true) $dom = $dom->getElementsByTagName('body')[0];
+    $elements = [];
+    foreach($dom->childNodes as $key => $element) {
+        $elements[$key] = $element;
         if ($element->hasChildNodes()) {
             $domIterator = new RecursiveIteratorIterator(
                 new RecursiveDOMIterator($element), RecursiveIteratorIterator::SELF_FIRST
             );
-            foreach($domIterator as $iteratorKey => $iteratorNode) {
-                $elements[$key]['child_nodes'][] = [
-                    'name' => $iteratorNode->nodeName,
-                    'attrs' => getElementAttributes($iteratorNode),
-                    'content' => $iteratorNode->textContent
-                ];
+            foreach($domIterator as $iteratorNode) {
+                $elements[$key]->childs[] = $iteratorNode;
             }
         }
-
-        $elements[$key]['name'] = $element->nodeName;
-        $elements[$key]['attrs'] = getElementAttributes($element);
-        $elements[$key]['child_nodes_count'] = ($element->childNodes) ? count($element->childNodes) : 0;
-        $elements[$key]['content'] = $element->textContent;
-
-        $element->nodeValue = $testIndex; 
-        $testIndex++;
     }
 
-    $markup = prepareTranslationMarkup($elements);
-    $translationStr = prepareTranslationString($elements);
-    var_dump($translationStr);
-    print_r($elements);
-    print_r($markup);
-    $restored = restoreTranslationContent($translationStr, $markup);
+    return $elements;
 }
 
-function prepareTranslationComponents($elements) {
-    $translationComponents = [
-        'markup' => [],
-        'content_parts' => [],
-        'output_blueprint' => ''
-    ];
+function prepareTranslationString($elements) : string {
+    $translationArray = [];
     $blockLevelIndex = 0;
 
     foreach($elements as $element) {
-        if(isset($element['child_nodes'])) {
-            $translationComponents['markup'][$blockLevelIndex]['name'] = $element['name'];
-            $translationComponents['markup'][$blockLevelIndex]['attrs'] = $element['attrs'];
-            foreach($element['child_nodes'] as $childElement) {
-                if ($childElement['name'] != "#text") {
-                    $translationComponents['markup'][$blockLevelIndex]['child_nodes'][] = [
-                        'name' => $childElement['name'],
-                        'attrs' => $childElement['attrs']
-                    ];
-                }
-                else {
-                    $translationComponents['content_parts'][] = refineTextContent($childElement['content']);
+        if(isset($element->childs)) {
+            foreach($element->childs as $childElement) {
+                if ($childElement->nodeName == "#text") {
+                    $translationArray[] = refineTextContent($childElement->textContent);
                 }
             }
             $blockLevelIndex++;
         }
     }
-
-    return $translationComponents;
-}
-
-function prepareTranslationMarkup($elements) {
-    $markupArray = prepareTranslationComponents($elements)['markup'];
-    
-    return $markupArray;
-}
-
-function prepareTranslationString($elements) {
-    $translationArray = prepareTranslationComponents($elements)['content_parts'];
-    print_r($translationArray);
     
     $translationStr = implode(DELIMITER, $translationArray);
     return $translationStr;
 }
 
-function refineTextContent($content) {
+function refineTextContent($content) : string {
     return str_replace(DELIMITER, '\\' . DELIMITER, htmlspecialchars_decode($content));
 }
 
-function restoreTranslationContent($translationString, $markup) {
+function restoreTranslationContent($translationString, DOMDocument $blueprint) {
     $test = preg_split('~(?<!\\\)' . preg_quote(DELIMITER, '~') . '~', $translationString);
-    print_r($test);
+    $output = $blueprint->saveHTML();
+    $key = 0;
+    foreach($test as $value) {
+        // istestirati (dodano zbog slucaja gdje google translate sam dodaje uzastopne tagove/delimitere)
+        if (!empty($value)) {
+            $output = str_replace("[$key]", $value, $output);
+            $key++;
+        }
+    }
+
+    return $output;
 }
 
-function getElementAttributes(DOMNode $element) {
+function getElementAttributes(DOMNode $element) : array {
     $attributes = [];
     if ($element->attributes) {
         for($i = 0; $i < $element->attributes->length; $i++) {
@@ -161,7 +148,6 @@ function getElementAttributes(DOMNode $element) {
             ];
         }
     }
-
     return $attributes;
 }
 
